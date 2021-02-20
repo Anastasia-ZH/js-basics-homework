45ead09ccf830f2a0b475c326882a292a0e984f7
Anastasia-ZH <nastya-spark@inbox.ru>
2021-02-20 22:26:59 +0300

    Task 2 done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
-
 const button = document.getElementById('calculate');
+const cloneButton = document.getElementById('clone');
 
-button.addEventListener('click', function () {
+
+/**
+ * calculate temperature
+ */
+function calculate() {
     let temperatureC = prompt('enter temperature');
 
     if (isNaN(parseInt(temperatureC))) {
@@ -11,4 +15,18 @@ button.addEventListener('click', function () {
 
     let temperatureF = parseInt((9 / 5) * temperatureC + 32);
     alert("Temperature in F: " + temperatureF);
-})
+}
+
+/**
+ * clone variables
+ */
+function clone() {
+    let name = "Вася"
+    let admin = name;
+    console.log(admin);
+}
+
+
+button.addEventListener('click', calculate)
+cloneButton.addEventListener('click', clone);
+
