6d87984f239a9f3b5c8a276ae5f6010e8d3cafa9
Anastasia-ZH <nastya-spark@inbox.ru>
2021-02-20 22:06:38 +0300

    Task 1 done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
-window.ready(function (){
 
+const button = document.getElementById('calculate');
+
+button.addEventListener('click', function () {
+    let temperatureC = prompt('enter temperature');
+
+    if (isNaN(parseInt(temperatureC))) {
+        alert('this is not a number');
+        return;
+    }
+
+    let temperatureF = parseInt((9 / 5) * temperatureC + 32);
+    alert("Temperature in F: " + temperatureF);
 })
